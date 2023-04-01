c287eff0aa37ace6c74a883c75b01c6ba5e6ce0f
Goggwell <zulkarnaensamuel1@gmail.com>
2023-04-01 18:34:11 +0700

    fix(trarrow): svg causing hydration mismatch

--- apps/web/components/Icons/TRArrow.tsx
+++ apps/web/components/Icons/TRArrow.tsx
@@ -4,7 +4,7 @@ const TRArrow = () => {
       width="20px"
       height="20px"
       viewBox="0 0 24 24"
-      stroke-width="2.5"
+      strokeWidth="2.5"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       color="#ffffff"
@@ -12,9 +12,9 @@ const TRArrow = () => {
       <path
         d="M6 19L19 6m0 0v12.48M19 6H6.52"
         stroke="#ffffff"
-        stroke-width="2.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></path>
     </svg>
   );
