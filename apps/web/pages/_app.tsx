09edc512cded3b5e3c5d9718886903e3f61b1a8c
Goggwell <zulkarnaensamuel1@gmail.com>
2023-04-01 13:04:22 +0700

    chore: adding next-seo plugin

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -3,11 +3,17 @@ import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 import "../styles/globals.scss";
 
+import { DefaultSeo } from "next-seo";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo
+        title="NihonGo"
+        description="NihonGO! is a language school that aims to give you all the necessary skills to thrive in a Japanese environment."
+      />
       <style jsx global>
         {`
           html,
