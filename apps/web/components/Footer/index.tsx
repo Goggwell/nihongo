1b634bdeee095572c4e23d971187fdd81aa5fb09
Goggwell <zulkarnaensamuel1@gmail.com>
2023-06-15 18:35:16 +0700

    feat(footer): styling

--- apps/web/components/Footer/index.tsx
+++ apps/web/components/Footer/index.tsx
@@ -5,30 +5,38 @@ import styles from "./Footer.module.scss";
 const Footer = () => {
   return (
     <footer className={styles.Footer}>
-      <div className={styles.Footer__container}>
-        <div className={clsx(styles.Footer__section, styles.logo)}>
-          <Link href="/">
-            <li className={styles.Footer__logo}>
-              <span>
-                NIHON<b>GO!</b>
-              </span>
-            </li>
-          </Link>
+      <div className={styles.Footer__section}>
+        <div className={styles.Footer__logo}>
+          <span>
+            Nihon<b>GO!</b>
+          </span>
         </div>
-        <div className={clsx(styles.Footer__section, styles.list)}>
-          <ul className={styles.Footer__list}>
-            <li className={styles["Footer__list--item"]}>
-              <Link href="/about">
-                <span>About</span>
-              </Link>
-            </li>
-            <li className={styles["Footer__list--item"]}>
-              <Link href="/contact">
-                <span>Contact</span>
-              </Link>
-            </li>
-          </ul>
-        </div>
+        <ul className={styles.Footer__links}>
+          <li className={styles.Footer__link}>
+            <Link href="/courses">
+              <span>Courses</span>
+            </Link>
+          </li>
+          <li className={styles.Footer__link}>
+            <Link href="/about">
+              <span>About</span>
+            </Link>
+          </li>
+          <li className={styles.Footer__link}>
+            <Link href="/blog">
+              <span>Blog</span>
+            </Link>
+          </li>
+          <li className={styles.Footer__link}>
+            <Link href="/contact">
+              <span>Contact</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.Footer__separator} />
+      <div className={clsx(styles.Footer__section, styles.legal)}>
+        <span className={styles.Footer__copyright}>&#169; 2023 NihonGO!</span>
       </div>
     </footer>
   );
