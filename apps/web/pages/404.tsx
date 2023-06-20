be7ca3a333f9874353c94ef8981d333874822ede
samuel-bd <samuely@frisseblikken.com>
2023-06-20 16:24:03 +0700

    feat(Blog): init page

--- apps/web/pages/404.tsx
+++ apps/web/pages/404.tsx
@@ -1,22 +1,3 @@
-import { useEffect } from "react";
-import groq from "groq";
-import { client } from "@/lib/client";
-
-export default function Custom404({ posts }) {
-  useEffect(() => {
-    console.log(posts);
-  }, [posts]);
-
+export default function Custom404() {
   return <h1>Hello!</h1>;
-}
-
-export async function getStaticProps() {
-  const posts = await client.fetch(groq`
-    *[_type == "post"]
-  `);
-  return {
-    props: {
-      posts,
-    },
-  };
 }
--- apps/web/pages/blog.tsx
+++ apps/web/pages/blog.tsx
@@ -1,9 +1,60 @@
-import styles from "@/styles/Blog.module.scss";
+import Link from "next/link";
+import groq from "groq";
+import { client } from "@/lib/client";
 
-export default function Blog() {
+import { inter, basierSquare } from "@/fonts/fonts";
+
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+import clsx from "clsx";
+import styles from "@/styles/Blog.module.scss";
+import { useEffect } from "react";
+
+export interface IPost {
+  _id?: string;
+  title?: string;
+  slug?: {
+    _type: "slug";
+    current: string;
+  };
+  publishedAt?: string;
+}
+
+export default function Blog({ posts }) {
+  useEffect(() => {
+    console.log(posts);
+  }, [posts]);
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className={clsx(styles.Blog, inter.variable, basierSquare.variable)}>
+      <Navbar changed={true} />
+      <div className={styles.Blog__container}>
+        {posts.length > 0 &&
+          posts.map(
+            ({ _id, title, slug, publishedAt = "" }: IPost) =>
+              slug && (
+                <li key={_id}>
+                  <Link href="/post/[slug]" as={`/post/${slug.current}`}>
+                    {title}
+                  </Link>{" "}
+                  ({new Date(publishedAt).toDateString()})
+                </li>
+              )
+          )}
+      </div>
+      <Footer />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const posts = await client.fetch(groq`
+    *[_type == "post" && publishedAt < now()] | order(publishedAt desc)
+  `);
+  return {
+    props: {
+      posts,
+    },
+  };
 }
