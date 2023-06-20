bd8cf36a4418b118727c02c3c297d90518ec228c
samuel-bd <samuely@frisseblikken.com>
2023-06-20 16:36:08 +0700

    chore: refine data handling

---
+++ apps/web/lib/api.ts
@@ -0,0 +1,33 @@
+import { client } from "./client";
+
+const getUniquePosts = (posts) => {
+  const slugs = new Set();
+  return posts.filter((post) => {
+    if (slugs.has(post.slug)) {
+      return false;
+    } else {
+      slugs.add(post.slug);
+      return true;
+    }
+  });
+};
+
+const postFields = `
+    _id,
+    name,
+    title,
+    'date': publishedAt,
+    excerpt,
+    'slug': slug.current,
+    'coverImage': mainImage,
+    'author': author->{name, 'picture': image.asset->url},
+`;
+
+export async function getAllPosts() {
+  const posts = await client.fetch(`
+        *[_type == "post"] | order(publishedAt desc){
+            ${postFields}
+        }
+    `);
+  return getUniquePosts(posts);
+}
--- apps/web/pages/blog.tsx
+++ apps/web/pages/blog.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import groq from "groq";
 import { client } from "@/lib/client";
+import { getAllPosts } from "@/lib/api";
 
 import { inter, basierSquare } from "@/fonts/fonts";
 
@@ -9,52 +10,23 @@ import Footer from "@/components/Footer";
 
 import clsx from "clsx";
 import styles from "@/styles/Blog.module.scss";
-import { useEffect } from "react";
-
-export interface IPost {
-  _id?: string;
-  title?: string;
-  slug?: {
-    _type: "slug";
-    current: string;
-  };
-  publishedAt?: string;
-}
 
 export default function Blog({ posts }) {
-  useEffect(() => {
-    console.log(posts);
-  }, [posts]);
-
   return (
     <div className={clsx(styles.Blog, inter.variable, basierSquare.variable)}>
       <Navbar changed={true} />
-      <div className={styles.Blog__container}>
-        {posts.length > 0 &&
-          posts.map(
-            ({ _id, title, slug, publishedAt = "" }: IPost) =>
-              slug && (
-                <li key={_id}>
-                  <Link href="/post/[slug]" as={`/post/${slug.current}`}>
-                    {title}
-                  </Link>{" "}
-                  ({new Date(publishedAt).toDateString()})
-                </li>
-              )
-          )}
-      </div>
+      <div className={styles.Blog__container}></div>
       <Footer />
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const posts = await client.fetch(groq`
-    *[_type == "post" && publishedAt < now()] | order(publishedAt desc)
-  `);
+  const posts = await getAllPosts();
   return {
     props: {
       posts,
     },
+    revalidate: 1,
   };
 }
